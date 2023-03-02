3936606d73af319d888d1105a0a166f45c24f6cd
ghislain <genet.ghislain@gmail.com>
2023-03-02 18:04:53 +0100

    1st commit

---
+++ script_1.js
@@ -0,0 +1,7 @@
+console.log("Bonjour monde");
+
+let name = prompt("Comment t'appelles-tu ?");
+
+
+console.log(`Bonjour ${name}`);
+
---
+++ script_2.js
@@ -0,0 +1,8 @@
+var total = 0;
+let number = prompt("De quel nombre veux-tu calculer la factorielle ?");
+for(let number; number =1; number -= 1)
+{
+  number *= number;
+}
+
+console.log(total);
